851c03f7a20137467ee5f197a47f4a7fe1b0f035
playbahn <plabanroy69@gmail.com>
2024-02-13 01:47:21 +0530

    stage3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-// Uncomment this block to pass the first stage
-use std::{io::Write, net::{TcpListener, TcpStream}};
+use std::io::ErrorKind;
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+const PONG: &[u8; 7] = b"+PONG\r\n";
+const BIND_ADDR: &str = "127.0.0.1:6379";
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    let tcp_listener: TcpListener = match TcpListener::bind("127.0.0.1:6379") {
+    let tcp_listener: TcpListener = match TcpListener::bind(BIND_ADDR) {
         Ok(tcp_listener) => tcp_listener,
         Err(e) => panic!("BIND ERROR: {:#?}", e.kind()),
     };
@@ -15,22 +18,40 @@ fn main() {
     for stream in tcp_listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("accepted new connection");
+                println!("Accepted new connection");
                 handle_connection(stream);
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Unsuccessful connection to TcpStream: {e}\r\nMoving on to next.");
             }
         }
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let pong: &str = "+PONG\r\n";
+    // *1\r\n$4\r\nping\r\n
+    let mut buffer: [u8; 512] = [0; 512];
+    let mut count: usize = 0;
+    loop {
+        match stream.read(&mut buffer) {
+            Err(e) if e.kind() == ErrorKind::Interrupted => continue,
 
-    if let Err(e) = stream.write_all(pong.as_bytes()) {
-        println!("{}",e);
-    } else if let Err(e) = stream.flush() {
-        println!("{}",e);
+            Err(e) => eprintln!("TcpStream read error: {e}"),
+
+            Ok(0) => break,
+
+            Ok(bytes_read) => {
+                count += bytes_read;
+                if count != count % 14 {
+                    if let Err(e) = stream.write_all(PONG) {
+                        eprintln!("Error sending response: {e}");
+                    } else if let Err(e) = stream.flush() {
+                        eprintln!("Error flushing tcpstream: {e}");
+                    }
+                }
+
+                count %= 14;
+            },
+        }
     }
 }
